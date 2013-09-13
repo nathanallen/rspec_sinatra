a2fc394c79f56da8c2931bfbb161e8014316d4ba
Apprentice <apprentice@devbootcamp.com>
2013-09-12 19:28:43 -0700

    refactor active record validations

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,13 +1,13 @@
 class Menu < ActiveRecord::Base
-  validates :name, :start_at, :presence => true
-  before_save :end_date_must_be_after_start_date
+  validates_presence_of :name, :start_at
+  validate :end_date_must_be_after_start_date
   before_save :titilize_title
   has_many :menu_items
 
   private
 
   def end_date_must_be_after_start_date
-    if !(self.end_at > self.start_at)
+    if self.end_at.present? && self.end_at < self.start_at
       errors.add(:end_at, "end date must be after start date")
     end
   end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -8,7 +8,6 @@ describe Menu do
       menu.end_at = Time.now - 1.day
       menu.save
       expect(menu.errors.messages[:end_at]).to include("end date must be after start date")
-      p menu
     end
   end
 
@@ -26,10 +25,3 @@ describe Menu do
     end
   end
 end
-
-
- # it "requires a name" do
- #      menu.name = nil
- #      menu.save
- #      expect(menu.errors.full_messages).to include("Name can't be blank")
- #    end
